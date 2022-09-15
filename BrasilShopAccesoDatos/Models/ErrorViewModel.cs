55580152530a5804bb357c6e2929eec9e411358c
nelso <nelso@NATALY>
2022-09-15 16:57:52 -0600

    Add project files.

---
+++ BrasilShopAccesoDatos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrasilShopAccesoDatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
